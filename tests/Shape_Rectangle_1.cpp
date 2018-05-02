7ef3ead7e29bc9a8a0434b1bddcdb15600b9d2d6
Minh Triet Pham Tran <trietptm@gmail.com>
2018-05-02 18:54:00 +0700

    Add files via upload

---
+++ tests/Shape_Rectangle_1.cpp
@@ -0,0 +1,40 @@
+// Shape_Rectangle.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+class Shape {
+	public:
+		void setWidth(int w) {
+			width = w;
+		}
+		void setHeight(int h) {
+			height = h;
+		}
+	protected:
+		int width;
+		int height;
+};
+
+class Rectangle : public Shape {
+	public:
+		int getArea() {
+			return (width * height);
+		}
+};
+
+int main()
+{
+	Rectangle Rect;
+
+	Rect.setWidth(5);
+	Rect.setHeight(7);
+
+	cout << "Total area: " << Rect.getArea() << endl;
+
+	return 0;
+}
+
